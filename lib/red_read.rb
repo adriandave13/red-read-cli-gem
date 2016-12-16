210b685b9d2be573be9480ec0240eaa48be83cd3
Adrian Dave <adrian@paradoxlogic.co>
2016-12-15 23:42:28 -0500

    Added dependencies

--- lib/red_read.rb
+++ lib/red_read.rb
@@ -1,5 +1,10 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative "red_read/version"
 require_relative "red_read/cli"
+require_relative "red_read/reddits"
 
 module RedRead
   # Your code goes here...
